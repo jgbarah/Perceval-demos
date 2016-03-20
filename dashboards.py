a5ecff9f42d4c42a737657b6128e7eb3c38ea754
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2016-03-21 00:25:54 +0100

    Now I can download all visualizations and searches for dashboards.

--- dashboards.py
+++ dashboards.py
@@ -32,6 +32,8 @@ dashboards.py --list --es_url http://localhost:9200
 
 import argparse
 import elasticsearch
+import json
+import sys
 import logging
 
 # Description of the documents of interest
@@ -54,6 +56,12 @@ def parse_args ():
                         choices=documents.keys(),
                         help="List available elements of given type"
                             + " (default: all)")
+    parser.add_argument("-g", "--get",
+                        type=str, nargs="+",
+                        help="Get specified dashboards")
+    parser.add_argument("-f", "--file",
+                        type=str,
+                        help="File to save / retrieve dashboards information")
     parser.add_argument("-e", "--es_url",
                         help = "ElasticSearch url where Kibana indexes are stored (http://user:secret@host:port/res)")
     parser.add_argument("-i", "--kibana_index", default=".kibana",
@@ -117,6 +125,77 @@ def list(es, index, kinds):
         print(documents[kind]+':')
         print_elements(elements)
 
+def get_dashboard(es, index, id):
+
+    document = es.get(index=index, doc_type='dashboard', id=id)
+    logging.debug('Retrieved dashboard document: ' + str(document))
+    visualization_ids = []
+    if 'panelsJSON' in document['_source']:
+        visualizations = json.loads(document['_source']['panelsJSON'])
+        for visualization in visualizations:
+            logging.debug('Visualization description: ' + str(visualization))
+            visualization_ids.append(visualization['id'])
+    return document, visualization_ids
+
+def get_visualization(es, index, id):
+
+    document = es.get(index=index, doc_type='visualization', id=id)
+    logging.debug('Retrieved visualization document: ' + str(document))
+    if 'savedSearchId' in document['_source']:
+        # Visualization based in a search, let's find its id
+        search = document['_source']['savedSearchId']
+    else:
+        search = None
+    return document, search
+
+def get_search(es, index, id):
+
+    document = es.get(index=index, doc_type='search', id=id)
+    logging.debug('Retrieved search document: ' + str(document))
+    return document
+
+def get(es, index, dashboards):
+    """Get a list of dashboards, with all their elements, from ElasticSearch.
+
+    """
+
+    elements = {
+        'dashboards': {},
+        'visualizations': {},
+        'searches': {}
+        }
+    for dashboard in dashboards:
+        logging.info('Getting dashboard: ' + dashboard)
+        (document, visualizations) = get_dashboard(es=es, index=index,
+                                                    id=dashboard)
+        elements['dashboards'][dashboard] = document
+        logging.debug('Found visualizations: ' + str(visualizations))
+        for visualization in visualizations:
+            if visualization not in elements['visualizations']:
+                logging.info('Getting visualization: ' + visualization)
+                (document, search) = get_visualization(es=es, index=index,
+                                                        id=visualization)
+                elements['visualizations'][visualization] = document
+                if search and (search not in elements['searches']):
+                    logging.info('Getting search: ' + search)
+                    document = get_search(es=es, index=index, id=search)
+                    elements['searches'][search] = document
+    logging.debug('Dashboards:')
+    logging.debug(elements['dashboards'])
+    logging.debug('Visualizations:')
+    logging.debug(elements['visualizations'])
+    logging.debug('Searches:')
+    logging.debug(elements['searches'])
+    return elements
+
+def save(file, elements):
+
+    if file is None:
+        json.dump(elements, sys.stdout, sort_keys=True, indent=4)
+    else:
+        with open(file, 'w') as fp:
+            json.dump(elements, fp, sort_keys=True, indent=4)
+
 if __name__ == "__main__":
     args = parse_args()
     if args.logging:
@@ -131,6 +210,13 @@ if __name__ == "__main__":
         else:
             logging.basicConfig(format=log_format, level=level)
 
+    if args.file:
+        file = args.file
+    else:
+        file = None
     es = elasticsearch.Elasticsearch([args.es_url])
     if args.list is not None:
         list(es=es, index=args.kibana_index,kinds=args.list)
+    if args.get is not None:
+        elements = get(es=es, index=args.kibana_index,dashboards=args.get)
+        save(args.file, elements)
